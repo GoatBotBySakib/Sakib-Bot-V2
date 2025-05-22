eb696fc76192818ee17e8765aa84997bf931d2ac
GoatBotBySakib <fbmarketingbysakib1@gmail.com>
2025-05-22 13:07:49 +0600

    Add files via upload

--- Script/commands/auto.js
+++ Script/commands/auto.js
@@ -1,6 +1,6 @@
 module.exports = {
  config:{
- name: "autodl",
+ name: "auto",
  version: "0.0.2",
  hasPermssion: 0,
  credits: "SHAON",
@@ -16,12 +16,12 @@ handleEvent: async function ({ api, event, args }) {
  const fs = require("fs-extra")
  const content = event.body ? event.body : '';
  const body = content.toLowerCase();
- const { alldown } = require("shaon-videos-downloader")
+ const {alldl} = require("shaon-videos-downloader")
  if (body.startsWith("https://")) {
  api.setMessageReaction("⚠️", event.messageID, (err) => {}, true);
-const data = await alldown(content);
+const data = await alldl(content);
  console.log(data)
- let Shaon = data.url;
+ let Shaon = data.videos[0].url;
  api.setMessageReaction("☢️", event.messageID, (err) => {}, true);
  const video = (await axios.get(Shaon, {
  responseType: "arraybuffer",
@@ -29,9 +29,7 @@ const data = await alldown(content);
  fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
 
  return api.sendMessage({
- body: `🔥🚀 𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | ᵁᴸᴸ⁴ˢᴴ 🔥💻 
-📥⚡𝗔𝘂𝘁𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿⚡📂
-🎬 𝐄𝐧𝐣𝐨𝐲 𝐭𝐡𝐞 𝐕𝐢𝐝𝐞𝐨 🎀`,
+ body: `VIDEO DOWNLOADED BY SAKIB 🎀`,
  attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
 
  }, event.threadID, event.messageID);
